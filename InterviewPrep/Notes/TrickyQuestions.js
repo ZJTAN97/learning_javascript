4e4ee1ccbb61cd5e9b8840b3e322214a68c620a6
ZJ <68308477+ZJTAN97@users.noreply.github.com>
2022-01-27 10:57:01 +0800

    added tricky questions

---
+++ InterviewPrep/Notes/TrickyQuestions.js
@@ -0,0 +1,72 @@
+// Tricky Question 1
+(function() {
+    var a = b = 3;
+    // is the same as
+    // var b = 3;
+    // var a = b;
+}) ();
+
+console.log("a defined? " + (typeof a !== 'undefined')) // false
+console.log("a defined? " + (typeof b !== 'undefined')) // true
+
+
+
+// Tricky Question 2
+var myObject = {
+    foo: "bar",
+    func: function() {
+        var self = this;
+        console.log("outer func:  this.foo = " + this.foo); // bar
+        console.log("outer func:  self.foo = " + self.foo); // bar
+        (function() {
+            console.log("inner func:  this.foo = " + this.foo); // undefined
+            console.log("inner func:  self.foo = " + self.foo); // bar
+        }());
+    }
+};
+myObject.func();
+
+
+
+// Tricky Question 3
+console.log(1 + + "2" + "2");
+
+
+// Tricky Question 4
+var a={},
+    b={key:'b'},
+    c={key:'c'};
+
+a[b]=123;
+a[c]=456;
+
+console.log(a);
+console.log(a[b]);
+
+
+
+// Tricky Question 5
+console.log('----- Question 5 ------')
+var length = 10;
+function fn() {
+	console.log(this.length);
+}
+
+var obj = {
+  length: 5,
+  method: function(fn) {
+    fn();
+    arguments[0]();
+  }
+};
+
+obj.method(fn, 1);
+
+
+
+// Tricky Question 6
+console.log('----- Question 6 ------')
+for (let i = 0; i < 5; i++) {
+    console.log('hi')
+    setTimeout(function() { console.log(i); }, i * 1000 );
+  }

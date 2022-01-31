1fd36b0aa1563b8458ae148754863a8a121d7996
ZJ <68308477+ZJTAN97@users.noreply.github.com>
2022-02-01 00:57:40 +0800

    halfway through qn 57

---
+++ InterviewPrep/LeetCode/Medium/57_LetterCombinationsPhoneNumber.js
@@ -0,0 +1,44 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+ const letterCombinations = function(digits) {
+    const hash = {
+        2: "abc",
+        3: "def",
+        4: "ghi",
+        5: "jkl",
+        6: "mno",
+        7: "pqrs",
+        8: "tuv",
+        9: "wxyz",
+    }
+
+    let result = [];
+    
+    const firstNumSet = hash[digits.charAt(0)];
+
+
+    const backTrack = (index, str) => {
+
+        if(index === digits.length) {
+            result.push(str);
+            return;
+        }
+
+        for(let c of hash[digits[index]]) {
+            backTrack(index+1, str + c);
+        }
+
+
+
+    }
+
+    backTrack(0, "");
+
+    console.log(result);
+
+};
+
+
+letterCombinations("23");

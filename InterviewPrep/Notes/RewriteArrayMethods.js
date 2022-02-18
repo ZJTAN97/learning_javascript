740416c2a63f5b31736539a0558286b4780bdf62
ZJ <68308477+ZJTAN97@users.noreply.github.com>
2022-02-18 10:54:38 +0800

    LETS GO!!!!!

--- InterviewPrep/Notes/RewriteArrayMethods.js
+++ InterviewPrep/Notes/RewriteArrayMethods.js
@@ -1,17 +1,15 @@
 // forEach
 const arr = [1, 2, 3];
 
-const myForEach = (array, callback) => {
-    for(let i=0; i < array.length; i++) {
-        callback(array[i], i, array);
+Array.prototype.myForEach = function(callback){
+    //this keyword reference to the array itself
+    for (let i = 0; i < this.length; i++) {
+      callback(this[i], i, this)
     }
-}
-myForEach(arr, function(item, index, array) {
-    console.log(item, index, array);
-})
-arr.forEach((item, index, array) => {
-    console.log(item, index, array);
-})
+  }
+
+arr.myForEach((item) => console.log(item));
+
 
 
 console.log('-----------------------------')
@@ -33,3 +31,51 @@ const sum2 = arr.reduce((a, b) => a+b, 0);
 
 console.log(sum);
 console.log(sum2);
+
+
+console.log("---------------------------");
+
+// map
+Array.prototype.myMap = function(callback) {
+    const newArr = [];
+    for(let i=0; i<this.length; i++) {
+        let mappedItem = callback(this[i], i, this);
+        newArr.push(mappedItem);
+    }
+    return newArr;
+}
+
+arr.myMap((item) => console.log(item));
+
+
+console.log("---------------------------");
+
+
+// find
+Array.prototype.myFind = function(callback) {
+    let result;
+    for(let i=0; i<this.length; i++) {
+        let isFound = callback(this[i], i, this);
+        if(isFound) {
+            result = this[i];
+            break;
+        }
+    }
+    return result;
+}
+
+
+// filter
+Array.prototype.myFilter = function(callback) {
+    let result = [];
+    for(let i=0; i<this.length; i++) {
+        let isFiltered = callback(this[i], i, this);
+        if(isFiltered) {
+            result.push(this[i]);
+        }
+    }
+    return result;
+}
+
+const filtered = arr.myFilter(item => item > 1);
+console.log(filtered);

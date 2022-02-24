cae65d5dde250f34bda136fac5e3d8cbcffff261
ZJ <68308477+ZJTAN97@users.noreply.github.com>
2022-02-24 10:07:19 +0800

    i still dont get dynammic programming....

--- LeetCode/Medium/300_LongestIncreasingSubsequence.js
+++ LeetCode/Medium/300_LongestIncreasingSubsequence.js
@@ -4,14 +4,22 @@
  */
  var lengthOfLIS = function(nums) {
 
-    const dp = [];
-    for(let i=0; i<nums.length; i++) {
-        dp.push(1);
-        for(let j=0; j<i; j++) {
-            if(nums[j] < nums[i]) dp[i] = Math.max(dp[i], dp[j] + 1);
+    const dp = Array(nums.length).fill(1);
+    
+    for(let i=nums.length-1; i>=0; i--) {
+        for(let j=i+1; j<nums.length; j++) {
+            if(nums[i] < nums[j]) {
+                dp[i] = Math.max(dp[i], 1+dp[j]);
+            }
         }
     }
-
-    return nums.length ? Math.max.apply(null, lis) : 0;
+    
+    console.log(dp);
+    return Math.max(...dp);
 
 };
+
+const sample = [1, 2, 4 ,3];
+const sample2 = [10, 9, 2, 5, 3, 7, 101, 18];
+
+lengthOfLIS(sample2);
---
+++ LeetCode/Medium/5_LongestPalindromicSubstring.js
@@ -0,0 +1,43 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+ var longestPalindrome = function(s) {
+
+    let result = "";
+    let resultLength = 0;
+  
+    for(let i=0; i<s.length; i++) {
+        // odd length palindrome
+        let left = i, right = i;
+        while(left >= 0 && right < s.length && s[left] === s[right]) {
+            if ((right - left + 1) > resultLength) {
+                result = s.substring(left, right+1);
+                resultLength = right - left + 1;
+            }
+            left --;
+            right ++;
+        }
+
+        // for even length palindrome
+        left = i, right = i+1;
+        while(left >= 0 && right < s.length && s[left] === s[right]) {
+            if ((right - left + 1) > resultLength) {
+                result = s.substring(left, right+1);
+                resultLength = right - left + 1;
+            }
+            left --;
+            right ++;
+        }
+
+    }
+
+    console.log(result);
+    return result;
+    
+};
+
+
+
+const sample = "babad";
+longestPalindrome(sample);
